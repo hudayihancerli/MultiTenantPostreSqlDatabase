2a46d5cdc5c2a685bdd40babc3a5eda3e18c0afb
hudayihancerli <m.hudayihancerli@gmail.com>
2025-07-07 00:18:10 +0300

    multi  tenant test

---
+++ WebApi/Models/Module.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models;
+
+public class Module
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    
+    public ICollection<Tenant> Tenants { get; set; } = new List<Tenant>();
+
+}
